8f2dc022e36761052ed2166b88520784abbca0ac
Diego Vergara <dv6702682@gmail.com>
2023-09-11 23:01:01 -0500

    Añadiendo colaboradores

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -2,22 +2,56 @@ import "./Formulario.css"
 import Texto from "../Texto/Texto"
 import ListaOpciones from "../ListaOpciones"
 import Boton from "../Boton"
+import { useState } from "react"
 
-const Formulario = () => {
+const Formulario = (props) => {
 
-    //transformar datos para enviarlos
-    const manejarEnvio = (e) => {
+    const [nombre, actualizarNombre] = useState("");
+    const [puesto, actualizarPuesto] = useState("");
+    const [foto, actualizarFoto] = useState("");
+    const [equipo, actualizarEquipo] = useState("");
+    
+    const manejarEnvio = (e) => { //transformar datos para enviarlos
         e.preventDefault();
-        console.log("Manejar el envio", e);
+        console.log("Manejar el envio");
+        let datosAEnviar = {
+            nombre,
+            puesto,
+            foto,
+            equipo
+        };
+        console.log(datosAEnviar);
     };
-
-    return <section className = "formulario">
+    
+    return <section className = "formulario"> 
         <form onSubmit={manejarEnvio}>
             <h2>Rellena el formulario para crear el colaborador.</h2>
-            <Texto titulo = "Nombre" placeholder = "Ingresar nombre" required />
-            <Texto titulo = "Puesto" placeholder = "Ingresar puesto" required />
-            <Texto titulo = "Foto" placeholder = "Ingresar enlace de foto" required />
-            <ListaOpciones />
+            <Texto 
+                titulo = "Nombre" 
+                placeholder = "Ingresar nombre" 
+                required 
+                valor = {nombre} 
+                actualizarValor = {actualizarNombre}
+            />
+            <Texto 
+                titulo = "Puesto" 
+                placeholder = "Ingresar puesto" 
+                required 
+                valor = {puesto} 
+                actualizarValor = {actualizarPuesto}
+            />
+            <Texto 
+                titulo = "Foto" 
+                placeholder = "Ingresar enlace de foto" 
+                required 
+                valor = {foto} 
+                actualizarValor = {actualizarFoto}
+            />
+            <ListaOpciones 
+                valor = {equipo} 
+                actualizarEquipo = {actualizarEquipo}
+                equipos = {props.equipos}
+            />
             <Boton>
                 Crear
             </Boton>
